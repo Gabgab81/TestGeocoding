8f97c61b0024bed3d3c60e12bc5bca08d6ed6d67
gabriel roux <rouxgab81@gmail.com>
2022-08-10 16:21:34 -0400

    geocoding done

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -13,6 +13,12 @@ class RestaurantsController < ApplicationController
 
     def index
         @restaurants = Restaurant.all
+        @markers = @restaurants.geocoded.map do |restaurant|
+            {
+              lat: restaurant.latitude,
+              lng: restaurant.longitude
+            }
+          end
     end
 
     def show
